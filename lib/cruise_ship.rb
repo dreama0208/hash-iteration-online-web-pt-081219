68c35ce390713a0341e9f8a747ee00b0dc7b8998
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-23 00:29:08 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -18,8 +18,3 @@ def select_winner(passengers)
   winner
 end
 
-def happy_birthday(birthday_kids)
-  birthday_kids.each { |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
-  }
-end
