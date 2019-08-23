39de5d8563252632480f86cfdaef3feb44da57bc
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-23 00:31:41 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,5 +15,6 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each { |name, age|
     if age <= 12
       puts "Happy Birthday #{name}! You are now #{age} years old!"
+    end
   }
 end
